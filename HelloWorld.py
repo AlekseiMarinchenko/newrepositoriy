3ee7e3ce785328765c2c8b704b152bd10381899e
AlekseiMarinchenko <144683112+AlekseiMarinchenko@users.noreply.github.com>
2023-11-02 11:11:49 +0300

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,50 +1,33 @@
-polynomial = list[int|float]
-def monomial_to_string(coeff: float, d: int) -> str:
-    if d == 0:
-        return f'{coeff}'
-    elif d == 1:
-        return f'{coeff}x'
-    return f'{coeff}x^{d}'
-def to_string(p: polynomial):
-    monomials = []
-    for d in reversed(range(len(p))):  # идем по убыванию степеней
-        monomials.append(monomial_to_string(p[d], d))
-    result = ''
-    for i in monomials:
-        if i[0] != '0':
-            result += i + ' + '
-    result = result[:max(result.rfind('+'), result.rfind('-'))]
-    if result[:2] == '1x':
-        result = result[1:]
-    return result.replace(' + -', ' - ').replace(' 1x', ' x').replace('-1x', '-x')
-def to_latex(p: polynomial) -> str:
-    c = to_string(p)
-    b = ''
-    flag = True
-    for i in range(len(c) - 1):
-        if c[i] == '0' and c[i + 1] == 'x':
-            flag = False
-        elif c[i] == '+' or c[i] == '-':
-            flag = True
-        if flag == True:
-            b += c[i]
-    b = b.replace(' + - ', ' - ').replace(' - + ', ' + ')
-    z = ''
-    for i in range(len(b) - 1):
-        if b[i] == '^':
-            z += '^{'
-        elif b[i] + b[i + 1] == ' +':
-            z += '} '
-        elif b[i] + b[i + 1] == ' -':
-            z+= '} '
-        else:
-            z += b[i]
-    a = ''
-    for i in b[::-1]:
-        if i != '^':
-            a += i
-        else:
-            break
-    return z + a[:-1] + '}'
-
-
+def angle(a: point, b: point, c: point) -> float:
+    a1 = a
+    b1 = b
+    c1 = c
+    for i in a1:
+        if i == 0:
+            i = 1
+    for i in b1:
+        if i == 0:
+            i = 1
+    for i in c1:
+        if i == 0:
+            i = 1
+    ab=[b[0]-a[0], b[1]-a[1]]
+    lenght_ab=((ab[0]**2) + (ab[1]**2))**0.5
+    bc=[c[0]-b[0], c[1]-b[1]]
+    lenght_bc = ((bc[0]**2) + (bc[1]**2))**0.5
+    cos = ((ab[0]*bc[0]) + (ab[1]*bc[1])) / (lenght_ab * lenght_bc)
+    arc = math.acos(cos)
+    ugol = math.degrees(arc)
+    flagx = None
+    flagy = None
+    if (a1[0] <= 0) == (b1[0] <= 0) == (c1[0]<=0):
+        flagx = True
+    else:
+        flagx = False
+    if (a1[1] <= 0) == (b1[1] <= 0) == (c1[1]<=0):
+        flagy = True
+    else:
+        flagy = False
+    if flagy and flagx:
+        return ugol
+    return 180 - ugol
