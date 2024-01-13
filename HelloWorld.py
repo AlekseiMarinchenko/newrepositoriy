a39e0e819fbc0ff5993b0ec2bb35189c2ee4f12a
AlekseiMarinchenko <144683112+AlekseiMarinchenko@users.noreply.github.com>
2024-01-13 23:37:03 +0300

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,3 +9,30 @@ def favourite_number() -> int:
     return 1
 def sequence_element(n: int):
     return int((1 + (1 + 8*(n-1))**0.5)/2)
+
+
+
+#Задания с файлами у меня так сделаны:
+def unix2dos(filename: str):
+    f = open(filename, "r")
+    s = f.read()
+    new = s.replace(chr(10), chr(10) + chr(13))
+    with open (filename, "w") as f:
+        f.write('')
+        f.write(new)
+    f.close()
+
+
+def json2yaml_noexcept(filename):
+    import json
+    import yaml
+    from pathlib import Path
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        yaml_filename = Path(filename).with_suffix('.yaml')
+        with open(yaml_filename, 'w') as file:
+            yaml.dump(data, file, default_flow_style=False)
+        return data
+    except Exception as ex:
+        return {'exception': ex}
